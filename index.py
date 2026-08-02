f520fa396c305059ebdcb1a007bb5a74b1c5fcae
dhanasri-2006 <dhanasri.kanisetti25@aiml.sce.edu.in>
2026-08-03 04:48:06 +0530

    Fixed variable name bug

--- index.py
+++ index.py
@@ -1,7 +1,5 @@
 name = "GitHub Actions"
 
-print(f"Hello from {name}")
-print("CI Pipeline Executed Successfully")
 number = 10
 
-print("Square =", number*numbere)
+print("Square =", number * number)
